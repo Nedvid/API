5e53953c53c1fd68e013788f441599a982ffad8d
Nedvid <nedved117@gmail.com>
2017-10-21 18:10:03 +0200

    back-end (OAuth, login, register)

--- CookerAPI/App_Start/WebApiConfig.cs
+++ CookerAPI/App_Start/WebApiConfig.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Formatting;
 using System.Web.Http;
 
 namespace CookerAPI
@@ -9,8 +11,6 @@ namespace CookerAPI
     {
         public static void Register(HttpConfiguration config)
         {
-            // Web API configuration and services
-
             // Web API routes
             config.MapHttpAttributeRoutes();
 
@@ -19,6 +19,9 @@ namespace CookerAPI
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            var jsonFormatter = config.Formatters.OfType<JsonMediaTypeFormatter>().First();
+            jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
         }
     }
 }
